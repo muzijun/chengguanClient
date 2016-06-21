f6c45f1e03f802521e4acfd472cbc74b0394505c
yk731340933@163.com <kun528674>
2016-06-21 14:05:43 +0800

    更新

--- app/src/main/java/com/pactera/chengguan/config/RequestListener.java
+++
@@ -1,9 +0,0 @@
-package com.pactera.chengguan.config;
-
-/** 网络返回接口
- * Created by lijun on 2015/12/30.
- */
-public interface RequestListener {
-    public abstract void success(String reqUrl, Object result);
-    public abstract void fail();
-}
